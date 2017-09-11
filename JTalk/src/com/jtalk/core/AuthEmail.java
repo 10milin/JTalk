e69716c5114d44afce534b3d2f5cbc75dc57badd
JSLHRD <JSLHRD@211.194.125.83>
2017-09-11 17:36:27 +0900

    메일 오타 수정

--- JTalk/src/com/jtalk/core/AuthEmail.java
+++ JTalk/src/com/jtalk/core/AuthEmail.java
@@ -50,7 +50,7 @@ public class AuthEmail {
 			content = "<a href = 'http://localhost:8181/JTalk/pages/auth/auth.html?"
 					+ "email=" + email + "&link=" + input + "' target='_blank'>회원 인증</a>";
 		}else if(command.equals("find")) {
-			subject = "JTalk 임시 비밀번호 발금 메일입니다.";
+			subject = "JTalk 임시 비밀번호 발급 메일입니다.";
 			content = "임시 비밀번호 : [" + input + "]";
 		}
 		try {
