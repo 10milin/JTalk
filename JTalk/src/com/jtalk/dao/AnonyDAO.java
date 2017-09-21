92dd3b3ef0d6e6507b43064439cc3cfdc40d57bf
Administrator <Administrator@211.230.85.25>
2017-09-21 22:50:39 +0900

    대숲 DAO DTO

--- JTalk/src/com/jtalk/dao/AnonyDAO.java
+++ JTalk/src/com/jtalk/dao/AnonyDAO.java
@@ -1,8 +1,12 @@
 package com.jtalk.dao;
 
+import static com.jtalk.db.JdbcUtils.*;
+
+import java.sql.*;
 import java.util.ArrayList;
 
 import com.jtalk.dto.AnonyDTO;
+
 
 public class AnonyDAO {
 	private AnonyDAO(){}
@@ -14,8 +18,39 @@ public class AnonyDAO {
 	}
 
 	public ArrayList<AnonyDTO> getAllAnony() {
+		ArrayList<AnonyDTO> list = null;
+		AnonyDTO anony = null;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from anony order by num desc";
 		
-		return null;
+		try
+		{
+			list = new ArrayList<AnonyDTO>();
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next())
+			{
+				anony = new AnonyDTO(rs.getInt("num"),
+						rs.getString("content"),
+						rs.getTimestamp("writeDate"),
+						rs.getInt("awesome"));
+				list.add(anony);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			close(rs,pstmt,conn);
+		}
+		
+		return list;
 	}
 
 }
